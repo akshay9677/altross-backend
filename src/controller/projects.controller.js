0a4ff28672b169ec8f8530a0c7c0a69c14307328
Akshay Kannan <akshay@facilio.com>
2021-05-23 21:40:32 +0530

    Projects get module record default

--- src/controller/projects.controller.js
+++ src/controller/projects.controller.js
@@ -1,6 +1,9 @@
 import ModuleBase from "./moduleBase.controller"
 import { MODULES } from "../utils/moduleSchemas"
 
+import { isEmpty } from "../utils/validation"
+import { errorResponse } from "../utils/responsehandler"
+
 const LookupHash = {
   orgs: { ...MODULES.orgs },
   users: { ...MODULES.users },
@@ -14,6 +17,25 @@ class Projects extends ModuleBase {
       LookupHash,
       MODULES["projects"].name
     )
+  }
+  async getModuleRecord(req, res) {
+    try {
+      let { orgid } = req.headers
+      let currModel = this.getCurrDBModel(orgid)
+      let { id } = req.body
+      let params
+      if (id) params["id"] = id
+      let record = await currModel.findOne(params)
+
+      if (isEmpty(record)) throw new Error("No record found for that Id")
+
+      return res.status(200).json({
+        data: record,
+        error: null,
+      })
+    } catch (error) {
+      return res.status(200).json(errorResponse(error))
+    }
   }
 }
 
